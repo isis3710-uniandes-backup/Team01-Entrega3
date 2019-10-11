0b60db12cab388b974953107c808fc29181c9626
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-10-11 00:27:12 -0500

    Carpetas

---
+++ imports/ui/mainpage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class mainpage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
