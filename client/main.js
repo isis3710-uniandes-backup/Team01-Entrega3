f08fd5a4e631d52635b4b7b75c4927ecd5339d47
FjGonzaR <fj.gonzalez@uniandes.edu.co>
2019-10-11 13:53:59 -0500

    principals

--- client/main.js
+++ client/main.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
-
+import MainPage from  '../imports/ui/mainpage';
 Meteor.startup(() => {
+    render (<MainPage/>,document.getElementById("root"));
 });
 
--- imports/ui/mainpage.js
+++ imports/ui/mainpage.js
@@ -1,25 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../ui/styles/mainpage.css';
 
 
 export default class mainpage extends Component {
+    state = {
+        allEvents : [],
+        confirmEvents : [],
+        createdEvents : [], 
+        canceledEvents : []
+    }
     render() {
         return (
-            <div>
-                <div class="row">
-                    <div class="col-3">
-                        <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-                            <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-                            <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-                            <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+            <div id="main" className="container-fluid">
+                <div className="row fullrow">
+                    <div className="col-3 d-flex align-items-center" >
+                    <button class="btn btn-spovent" type="submit">Crear un nuevo evento</button>
+                        <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                            <a className="nav-link active" id="v-pills-all-tab" data-toggle="pill" href="#v-pills-all" role="tab" aria-controls="v-pills-all" aria-selected="true">Todos</a>
+                            <a className="nav-link" id="v-pills-willAssist-tab" data-toggle="pill" href="#v-pills-willAssist" role="tab" aria-controls="v-pills-willAssist" aria-selected="false">Asistiré</a>
+                            <a className="nav-link" id="v-pills-created-tab" data-toggle="pill" href="#v-pills-created" role="tab" aria-controls="v-pills-created" aria-selected="false">Creados</a>
+                            <a className="nav-link" id="v-pills-cancelados-tab" data-toggle="pill" href="#v-pills-cancelados" role="tab" aria-controls="v-pills-cancelados" aria-selected="false">Cancelados</a>
                         </div>
                     </div>
-                    <div class="col-9">
-                        <div class="tab-content" id="v-pills-tabContent">
-                            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">DWFevgwbHRENTJRMYKDTU,LYIGUO;</div>
-                            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-                            <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-                            <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                    <div className="col-9">
+                        <div className="tab-content" id="v-pills-tabContent">
+                            <div className="tab-pane fade show active" id="v-pills-all" role="tabpanel" aria-labelledby="v-pills-all-tab">{this.state.allEvents.map((e,i) => <H/> )}</div>
+                            <div className="tab-pane fade" id="v-pills-willAssist" role="tabpanel" aria-labelledby="v-pills-willAssist-tab">{this.state.confirmEvents.map((e,i) => <H/>)}</div>
+                            <div className="tab-pane fade" id="v-pills-created" role="tabpanel" aria-labelledby="v-pills-created-tab">{this.state.createdEvents.map((e,i) => <H/>)}</div>
+                            <div className="tab-pane fade" id="v-pills-cancelados" role="tabpanel" aria-labelledby="v-pills-cancelados-tab">{this.state.canceledEvents.map((e,i) => <H/>)}</div>
                         </div>
                     </div>
                 </div>
