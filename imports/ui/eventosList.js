82490df25a227a92663c80b1e6cd854f4de02ad2
Andres3 <a.manrique@uniandes.edu.co>
2019-10-14 22:19:48 -0500

    cambios de acuerdo al id

--- imports/ui/eventosList.js
+++ imports/ui/eventosList.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Event from './event';
+import { Users, Events } from "../api/mongoSettings";
 
 export default class EventosList extends React.Component {
     constructor(props) {
         super(props);
+        let eventos=[];
+        if(this.props.identificador===0){
+            eventos=Events.find({});
+        }else if(this.props.identificador===1){
+            eventos=Users.findOne({username: this.props.username}).subscribedEvents;
+        }else{
+            eventos=Users.findOne({username: this.props.username}).offeredEvents;
+        }
         this.state = {
             eventos: this.props.eventos,
-            joinFunction: this.props.joinFunction
+            joinFunction: eventos
         }
     }
 
